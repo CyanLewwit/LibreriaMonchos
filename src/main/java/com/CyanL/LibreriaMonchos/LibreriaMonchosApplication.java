efeafc95e2abfd8a7a8cb3f1d3abc9a8c83de837
Ian Yevara <ianyevara@gmail.com>
2021-11-15 22:37:31 -0300

    Primer Commit, el Inicio

---
+++ src/main/java/com/CyanL/LibreriaMonchos/LibreriaMonchosApplication.java
@@ -0,0 +1,13 @@
+package com.CyanL.LibreriaMonchos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaMonchosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaMonchosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CyanL/LibreriaMonchos/LibreriaMonchosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CyanL.LibreriaMonchos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaMonchosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
